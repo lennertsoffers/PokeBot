f981b142cc5ca3901c31038c656592ec3ab797b7
Lennert Soffers <r0833969@student.thomasmore.be>
2021-05-23 22:10:50 +0200

    wild battle first gen finished

--- main.py
+++ main.py
@@ -18,18 +18,16 @@ def chooseStarter():
     # while choice not in ["charmander", "bulbasaur", "squirtle", "rotom", "1", "4", "7"]:
     #     choice = input("Choose your starter: ").lower()
     starter = Pokemon(choice, level=38)
-    starter.lowerHp(starter.getHp() - 1)
     return starter
 
 
 newTrainer = createTrainer()
 pkm2 = Pokemon(random.randint(1, 500), 30)
-pkm2.lowerHp(pkm2.getHp() - 1)
 newTrainer.addPokemon(pkm2)
-# newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
-# newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
-# newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
-# newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
+newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
+newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
+newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
+newTrainer.addPokemon(Pokemon(random.randint(1, 500), 30))
 for PKM in newTrainer.getPokemonList():
     newTrainer.depositCarryPokemon(newTrainer.getPokemonList()[PKM])
 wildBattle(newTrainer)
